80dda002919dc5cadeb355208d6b545f951ca5e5
Jaxongir <jmuxutdinov@mail.ru>
2021-10-10 06:10:44 +0500

    install styled-components

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container } from './style'
+
+
+export const Navbar = () => {
+    return (
+        <Container>
+            Navbar
+        </Container>
+    )
+}
--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 
-
 export const Root = () => {
-  return (
-    <div></div>
-  );
+  return <div></div>;
 };
 export default Root;
