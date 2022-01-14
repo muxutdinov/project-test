88c5a5d76359aadea3726fc548af482ebbf32279
Muxutdinov <jmuxutdinov@mail.ru>
2022-01-14 21:04:36 +0500

    some changes

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,13 +2,12 @@ import React from "react";
 import '../root/style.css'
 import { Button } from "./Button";
 import "./HeroSection.css";
-// import video from '../assets/videos/video-2.mp4'
 import video from '../assets/videos/video.mp4'
 
 export const HeroSection = () => {
   return (
     <div className="hero-container">
-      <video src={video} avtoPlay loop muted />
+      <video src={video} autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
