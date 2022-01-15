f7cbebbcf2fc5941dbba7bcb29b31e272764ebcf
Muxutdinov <jmuxutdinov@mail.ru>
2022-01-15 10:43:28 +0500

    changed

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -14,13 +14,13 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src={img9}
+              img={img9}
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
               label='Adventure'
               path='/services'
             />
             <CardItem
-              src={img2}
+              img={img2}
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
@@ -28,19 +28,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src={img3}
+              img={img3}
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src={img4}
+              img={img4}
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src={img8}
+              img={img8}
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path='/sign-up'
